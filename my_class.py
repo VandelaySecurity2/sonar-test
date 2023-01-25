dd0a445b9b16ba1875ff99f642aeeba830a27895
shahar-legit <102029423+shahar-legit@users.noreply.github.com>
2023-01-25 16:17:23 +0200

    comment - testing push event for cso

--- my_class.py
+++ my_class.py
@@ -78,3 +78,5 @@ def init_ldap():
    connect.simple_bind_s('cn=root') # Noncompliant
    connect.bind_s('cn=root', None) # Noncompliant
    connect.bind('cn=root', None) # Noncompliant
+
+# just a comment
